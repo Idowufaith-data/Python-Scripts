7fee53d44687d0dc13e42241a1b56f420548736d
idowufaith-data <idowufaith4u@gmail.com>
2023-04-11 13:12:48 +0100

    Commit

--- Functions.py
+++ Functions.py
@@ -1,8 +1,8 @@
 # Function is a block of code that performs a particular task; function is specified with the 'def' keyword
 
 # Function creation
-def greetings_function():
-    print('Welcome User')
+def greetings_function(name):
+    print('Welcome', name)
 
 # Function call
-greetings_function()
+greetings_function('Faith')
