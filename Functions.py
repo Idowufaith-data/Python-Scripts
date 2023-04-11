5953d565029f021a4d55dbd6a2c99ee7f4bacc18
idowufaith-data <idowufaith4u@gmail.com>
2023-04-11 13:21:43 +0100

    commit

--- Functions.py
+++ Functions.py
@@ -1,8 +1,15 @@
 # Function is a block of code that performs a particular task; function is specified with the 'def' keyword
 
 # Function creation
-def greetings_function(name):
-    print('Welcome', name)
+def greetings_function(name, age):
+    print('Welcome '+name+ '. You are', age, 'years old')
 
 # Function call
-greetings_function(10)
+greetings_function('Faith', 20)
+
+# Function creation to greet just one user in a list/tuple containing many users
+def greetings_function(*names):
+    print('Welcome', names[1])
+
+# Function call
+greetings_function('Faith', 'Tim', 'Tom')
