aadb609fbddf094b3f032a61d4ea22a493fcdac5
idowufaith-data <idowufaith4u@gmail.com>
2023-04-11 13:30:50 +0100

    commit

--- Functions.py
+++ Functions.py
@@ -13,3 +13,12 @@ def greetings_function(*names):
 
 # Function call
 greetings_function('Faith', 'Tim', 'Tom')
+
+# Taking input from the user
+def greetings_function(name,age):
+    print('Welcome'+name+'. You are', age, 'years old')
+
+# Assigining the input function to the variables and calling the function
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+greetings_function(name,age)
