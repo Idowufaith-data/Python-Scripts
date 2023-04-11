616f3ec1ddc1689802447238429aa9edef03454a
idowufaith-data <idowufaith4u@gmail.com>
2023-04-11 13:11:42 +0100

    commit

---
+++ Functions.py
@@ -0,0 +1,8 @@
+# Function is a block of code that performs a particular task; function is specified with the 'def' keyword
+
+# Function creation
+def greetings_function():
+    print('Welcome User')
+
+# Function call
+greetings_function()
