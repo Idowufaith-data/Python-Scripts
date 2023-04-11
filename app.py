63d5fa7b366e1e68dd62742860388f6eb03eaac8
idowufaith-data <idowufaith4u@gmail.com>
2023-04-11 11:55:42 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,6 @@
+sentence = input("Enter your sentence: ")
+print('Your sentence is: ',sentence)
+
+word = input('Enter the word to replace: ')
+word1 = input('Enter the word to replace it with: ')
+print(sentence.replace(word,word1))
